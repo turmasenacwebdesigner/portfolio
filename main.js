aa65446c25f5f52183b508f1360971bec1a60b2e
turmasenacwebdesigner <103004240+turmasenacwebdesigner@users.noreply.github.com>
2022-04-19 20:51:58 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const LinksSocialMedia = {
+  github: 'turmasenacwebdesigner',
+  youtube: 'youtube',
+  facebook: 'facebook',
+  instagram: 'instagram',
+  twitter: 'twitter'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}` // crase ao abrir e fechar
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textcontent = data.name
+      userBio.textcontent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
